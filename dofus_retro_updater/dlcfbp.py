28720414c3b438467b052bfde2f51fa15fe8f16e
adann0 <skynbrrpz@gmail.com>
2021-05-13 02:31:48 +0200

    add exception when wifi is down

--- dofus_retro_updater/dlcfbp.py
+++ dofus_retro_updater/dlcfbp.py
@@ -11,10 +11,14 @@ USAGE: ./dlcfbp [filename] [url]
 import sys
 import re
 import cloudscraper
+import requests
 
 def download(filename, url, bar=False) :
     scraper = cloudscraper.create_scraper()
-    response = scraper.get(url, stream=True)
+    try :
+    	response = scraper.get(url, stream=True)
+    except requests.exceptions.ConnectionError:
+    	return(False) # wifi down
     if response.status_code == 200 :
         with open(filename, "wb") as f:
             # Progression bar from : https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
@@ -32,8 +36,7 @@ def download(filename, url, bar=False) :
                     sys.stdout.flush()
         return(True)
     else :
-        print("[-] Can't download the file.")
-        return(False)
+        return(False) # can't download the file
 
 if __name__ == "__main__" :
     if len(sys.argv) < 3 or not sys.argv[2].startswith("http") :
